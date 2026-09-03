6d2b48b8127698d5aa3b4d9085dcec7fde5f318c
Alejo Hidalgo <ajjhidalgo613@gmail.com>
2026-09-03 11:19:14 -0300

    firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,8 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.18.0"))
+    implementation("com.google.firebase:firebase-analytics")
     implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
     implementation(platform(libs.androidx.compose.bom))
